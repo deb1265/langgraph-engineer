c0777948c559a900a1d069e41312713a18fcb93a
deb1265 <117177026+deb1265@users.noreply.github.com>
2024-09-28 20:50:03 -0400

    Update model.py

--- src/langgraph_engineer/model.py
+++ src/langgraph_engineer/model.py
@@ -3,11 +3,11 @@ from langchain_openai import ChatOpenAI
 def _get_model(config, default, key):
     model = config['configurable'].get(key, default)
     
-    if model == "openai-oi-mini":
+    if model == "o1-mini":
         return ChatOpenAI(temperature=0, model_name="o1-mini")
-    elif model == "openai-oi-preview":
+    elif model == "o1-preview":
         return ChatOpenAI(temperature=0, model_name="o1-preview")
-    elif model == "openai-gpt4o":
+    elif model == "gpt-4o":
         return ChatOpenAI(temperature=0, model_name="gpt-4o")
     else:
         raise ValueError("Invalid model specified. Supported models: openai-oi-mini, openai-oi-preview, openai-gpt4o.")
